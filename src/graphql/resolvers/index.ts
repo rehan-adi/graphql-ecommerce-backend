e792bb79361d20236eacd952cb393167b2a4c12c
Rehan <rehanalire52@gmail.com>
2025-04-30 15:38:53 +0530

    refactor: restructure resolvers to separate Query and Mutation handlers

--- src/graphql/resolvers/index.ts
+++ src/graphql/resolvers/index.ts
@@ -2,6 +2,11 @@ import { AuthResolvers } from "./auth.resolvers.js";
 import { UserResolver } from "./user.resolvers.js";
 
 export const resolvers = {
-  ...AuthResolvers,
-  ...UserResolver,
+  Query: {
+    ...UserResolver.Query,
+  },
+  Mutation: {
+    ...AuthResolvers.Mutation,
+    ...UserResolver.Mutation,
+  },
 };
